0dc84f8a61a619297009b83e853b3d2a675d01dc
Firiyuu <phaophaoespina@gmail.com>
2021-09-03 12:40:31 +0800

    initial boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import Web3 from 'web3';
+import Identicon from 'identicon.js';
+import './App.css';
+import SocialNetwork from '../abis/SocialNetwork.json'
+import Navbar from './Navbar'
+import Main from './Main'
+
+class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+
+    const balanceWei = await web3.eth.getBalance(accounts[0]);
+    var balanceEth = await web3.utils.fromWei(balanceWei, 'ether')
+    this.setState({ balance: balanceEth })
+
+
+
+    // Network ID
+    const networkId = await web3.eth.net.getId()
+    const networkData = SocialNetwork.networks[networkId]
+    if(networkData) {
+      const socialNetwork = web3.eth.Contract(SocialNetwork.abi, networkData.address)
+      this.setState({ socialNetwork })
+      const postCount = await socialNetwork.methods.postCount().call()
+      this.setState({ postCount })
+      // Load Posts
+      for (var i = 1; i <= postCount; i++) {
+        const post = await socialNetwork.methods.posts(i).call()
+        this.setState({
+          posts: [...this.state.posts, post]
+        })
+      }
+      // Sort posts. Show highest tipped posts first
+      this.setState({
+        posts: this.state.posts.sort((a,b) => b.tipAmount - a.tipAmount )
+      })
+      this.setState({ loading: false})
+    } else {
+      window.alert('SocialNetwork contract not deployed to detected network.')
+    }
+  }
+
+  createPost(content) {
+    this.setState({ loading: true })
+    this.state.socialNetwork.methods.createPost(content).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  tipPost(id, tipAmount) {
+    this.setState({ loading: true })
+    this.state.socialNetwork.methods.tipPost(id).send({ from: this.state.account, value: tipAmount })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      balance: 0,
+      socialNetwork: null,
+      postCount: 0,
+      posts: [],
+      loading: true
+    }
+
+    this.createPost = this.createPost.bind(this)
+    this.tipPost = this.tipPost.bind(this)
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar account={this.state.account} balance={this.state.balance} />
+        { this.state.loading
+          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
+          : <Main
+              posts={this.state.posts}
+              createPost={this.createPost}
+              tipPost={this.tipPost}
+            />
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
